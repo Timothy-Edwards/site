b20666529f3f5f6f8aa1e3b721ea3614a6d8e84d
Tim <tim@edwards.org.au>
2020-12-18 20:42:59 +1100

    Change styling

--- pages/index.js
+++ pages/index.js
@@ -4,63 +4,109 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>🤠⌨️Tim Edwards</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Tim Edwards</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
-
-      <main>
-        <h1 className="title">
-          Tim Edwards
+      <h1 className="title">
+        Tim Edwards
         </h1>
+      <main>
 
         <div className="grid">
-        <img href="/bg.jpg" />
-          <a href="/posts/myPost" className="card">
-            <h3>ME first post &rarr;</h3>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a> <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is
+            what if there
+            was a Liberation
+            of contet>?
+            what if there
+            was a Liberation
+            of contet>?
+            what if there
+            was a Liberation
+            of contet>?
+            </p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
+            <p>This is where me first post is</p>
+          </a>
+          <a href="/posts/first-post" className="card">
+            <h3>First post &rarr;</h3>
             <p>This is where me first post is</p>
           </a>
         </div>
       </main>
       <footer>
-          🤠 ⌨️
+        🤠
       </footer>
 
       <style jsx>{`
+        ::-webkit-scrollbar {
+          width: 0px;  /* Remove scrollbar space */
+          background: transparent;  /* Optional: just make scrollbar invisible */
+        }
+
         .container {
           min-height: 100vh;
-          padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          background-color: #70a86a;
         }
 
         main {
-          padding: 5rem 0;
+          width: 100%;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          background-color: white;
+          overflow-y: scroll;
         }
 
         footer {
           width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
+          height: 50px;
           display: flex;
           justify-content: center;
           align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+          position: sticky;
+          bottom: 0px;
+          background-color: #70a86a;
         }
 
         a {
@@ -81,8 +127,17 @@ export default function Home() {
 
         .title {
           margin: 0;
+          height: 100px;
           line-height: 1.15;
           font-size: 4rem;
+          background-color: #70a86a;;
+          width: 100%;
+          top: 0px;
+          position: sticky;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-family: consolas;
         }
 
         .title,
@@ -94,24 +149,12 @@ export default function Home() {
           line-height: 1.5;
           font-size: 1.5rem;
         }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
         .grid {
           display: flex;
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-
           max-width: 800px;
-          margin-top: 3rem;
         }
 
         .card {
@@ -121,32 +164,30 @@ export default function Home() {
           text-align: left;
           color: inherit;
           text-decoration: none;
-          border: 1px solid #eaeaea;
           border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+          transition: color 0.15s ease, border-color 0.15s ease, background-color 0.15s ease;
           min-width: 300px;
         }
 
         .card:hover,
         .card:focus,
         .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+          background-color: lightblue;
         }
 
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
+          font-family: consolas;
         }
 
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
+          max-height: 4rem;
+          text-overflow: elipsis;
+          overflow-y: hidden;
         }
 
         @media (max-width: 600px) {
